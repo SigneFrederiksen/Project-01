091f66452dde8e1ad7ab3c74b8cc9f062eace2cd
Signe Frederiksen <signe_frederiksen@hotmail.dk>
2018-09-15 22:16:15 +0200

    Created Project-01

---
+++ Project-01/CustomerClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_01
+{
+    class CustomerClass
+    {
+        // Sets the fields to Private, 
+        // and declaring variables to the Class 'CustomerClass'. 
+        private string NameOfCustomer;
+        private int Age;
+        private int Zipcode;
+        private string Password;
+
+        // Adding a constructor method called 'CustomerClass()', to the 'CustomerClass'.
+        public CustomerClass()
+        {
+            // Initialize the fields of the Class ' CustomerClass'.
+            this.NameOfCustomer = "Signe Frederiksen";
+            this.Age = 23;
+            this.Zipcode = 2300;
+            this.Password = "124x53";
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + NameOfCustomer + ", Age: " + Age + " year old, Zipcode: " + Zipcode + ", Password: " + Password + "";
+        }
+    }
+}
---
+++ Project-01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerClass myFirstCustomer = new CustomerClass();
+
+            Console.WriteLine("My first customer is: " + myFirstCustomer.ToString());
+        }
+    }
+}
